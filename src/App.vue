082da272cc47a2e8d779032fd956215303a5ffa6
Md. Akramul Haque Rony <hsrony04@gmail.com>
2023-07-29 17:34:08 +0600

    My First Component
    First Component of Vue Js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Rony in Vue Js "/>
+  <HomeComponent/>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeComponent,
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Home Component</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeComponent',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+
+  <style scoped> 
+    h1
+    {
+        color:orange;
+    }
+</style>
